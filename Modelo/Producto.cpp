6e2c359d7dcaac3e6c068731085744e9dace48b6
ITALITIUS <88645224+ITALITIUS@users.noreply.github.com>
2022-11-15 23:39:49 -0500

    main
    Crear carpeta main , modelo de producto y directorios controlador , modelo , vista

---
+++ Modelo/Producto.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include <iostream>
+
+class Producto{
+    string nombre="manzana";
+public:
+    void Vernombre(){
+        cout <<"nombre" << nombre<<endl;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include "Modelo/Producto.cpp"
+using namespace std;
+
+int main ()
+{
+    cout << "hola mundo";
+    Producto* producto = new Producto;
+    producto->Vernombre();
+
+}
